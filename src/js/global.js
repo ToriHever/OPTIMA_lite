821a0ec4e0d0ce15a41b8edc1c30e9b9b93b5480
VI <vika.miroshnikova2016@gmail.com>
2024-11-01 15:48:41 +0300

    Скрипт меняет фавиконку при темной теме браузера

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,17 @@
+function updateFavicon() {
+    const favicon = document.getElementById('favicon');
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    favicon.href = isDarkMode ? '/src/assets/img/logo/logo-mini-darck.png' : '/src/assets/img/logo/logo-mini-light.png';
+}
+
+// Устанавливаем favicon при загрузке страницы
+updateFavicon();
+
+// Отслеживаем изменения в системе
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateFavicon);
+
+
 console.log('Работает?')
 const sandwichmenu = document.querySelector('#sandwichmenu');
 const js_menu = document.querySelector('#js_menu');
